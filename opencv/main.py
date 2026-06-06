70a80742421bd6e07d4d24e851ddb2abd6dc6733
paulmit000 <paul.mitanshu@gmail.com>
2026-06-06 15:23:36 -0500

    attempted fix for stream_url

--- opencv/main.py
+++ opencv/main.py
@@ -17,11 +17,16 @@ from camera import get_camera, detect_red
 
 
 if __name__ == "__main__":
+    print("STREAM_URL: " + STREAM_URL)
     camera = get_camera()
+    print("Camera object: " + str(camera))
+    print("Camera isOpened: " + str(camera.isOpened()))
 
     while True:
         ret, frame = camera.read()
+        print("camera.read() returned ret=" + str(ret))
         if not ret:
+            print("ret is False, breaking")
             break
 
         # Process the frame (e.g., display it)
@@ -32,6 +37,7 @@ if __name__ == "__main__":
             break
 
         detected, center = detect_red(frame)
+        print("detect_red() returned detected=" + str(detected))
         if detected:
             print("Red object detected at center: " + str(center))
             
